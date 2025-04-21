a67ae52a41d84b0239804e0dfef1d5c5895d7a9d
Shellishack <40737228+Shellishack@users.noreply.github.com>
2025-04-21 23:22:03 +0800

    Remove konva to avoid react-reconcile makes mismatched react versions (maybe move to main code)

--- src/component/code-editor-view.tsx
+++ src/component/code-editor-view.tsx
@@ -2,14 +2,10 @@ import ReactCodeMirror, {
   Extension,
   ReactCodeMirrorRef,
 } from "@uiw/react-codemirror";
-import { useCallback, useEffect, useRef, useState } from "react";
+import {  useEffect, useRef, useState } from "react";
 import { vscodeDark, vscodeLight } from "@uiw/codemirror-theme-vscode";
-import { DrawnLine } from "../lib/types";
-import CanvasEditor from "./canvas/canvas-editor";
-import html2canvas from "html2canvas";
 
 import React from "react";
-import { createRoot } from "react-dom/client";
 import { codeInlineSuggestionExtension } from "../lib/codemirror-extensions/code-inline-suggestion";
 import { getLanguageExtension } from "../lib/codemirror-extensions/get-language-extension";
 import { DelayedTrigger } from "../lib/delayed-trigger";
@@ -20,7 +16,7 @@ import {
   useNotification,
   useTheme,
 } from "@pulse-editor/react-api";
-import { FileViewModel, TextFileSelection } from "@pulse-editor/types";
+import { FileViewModel } from "@pulse-editor/types";
 import { Config } from "../main";
 import {
   InlineSuggestionAgent,
@@ -181,7 +177,6 @@ export default function CodeEditorView() {
     undefined
   );
   const { isReady, installAgent, runAgentMethod } = useAgents(moduleName);
-  const [isCanvasReady, setIsCanvasReady] = useState(false);
   // setup a timer for delayed saving
   const saveTriggerRef = useRef<DelayedTrigger | undefined>(
     new DelayedTrigger(200)
@@ -189,9 +184,6 @@ export default function CodeEditorView() {
   const { openNotification } = useNotification(moduleName);
   const { viewFile, updateViewFile , setIsLoaded} = useFileView(moduleName);
 
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [isDownloadClip, setIsDownloadClip] = useState(false);
-  const [isDrawHulls, setIsDrawHulls] = useState(false);
 
   const [cmFileExtension, setCmFileExtension] = useState<Extension | undefined>(
     undefined
@@ -218,69 +210,6 @@ export default function CodeEditorView() {
       setTheme(vscodeLight);
     }
   }, [pulseTheme]);
-
-  useEffect(() => {
-    // reset drawing info when drawing mode is off
-    if (!isDrawing) {
-      setViewDocument((prev) => {
-        // Return undefined if viewDocument is not set
-        if (!prev) {
-          return prev;
-        }
-        return {
-          ...prev,
-          selections: [],
-        };
-      });
-      setIsCanvasReady(false);
-      const canvasWidget = document.getElementById("canvas-widget");
-      if (canvasWidget) {
-        canvasWidget.remove();
-      }
-      return;
-    }
-    // Get editor canvas
-    const scroller = cmRef.current?.view?.scrollDOM;
-    if (!scroller) {
-      throw new Error("Scroller not found");
-    }
-    const editorContent = cmRef.current?.view?.contentDOM;
-    if (!editorContent) {
-      throw new Error("Editor content not found");
-    }
-    const editor = document.getElementsByClassName("cm-editor")[0];
-    if (!editor) {
-      throw new Error("Editor not found");
-    }
-    // Get the background color of the editor
-    const background = window.getComputedStyle(editor).backgroundColor;
-    // Convert the editor content to a canvas using html2canvas
-    html2canvas(editorContent, {
-      windowWidth: editorContent.offsetWidth,
-      windowHeight: editorContent.offsetHeight,
-      backgroundColor: background,
-    }).then((canvas) => {
-      // Set the canvas to the state
-      setIsCanvasReady(true);
-      /* This finds cm-scroller and injects the canvas.
-           This is doable because cm-scroller does not keep
-           track of its content (?).
-           It is known that injecting into cm-content will cause
-           CodeMirror to lose its internal states as the content
-           is directly managed by CodeMirror internals.
-           A better way to do this is to make a CodeMirror decoration
-           instead of injecting into its DOM. */
-      // Add the canvas to the editor content
-      scroller.appendChild(
-        getCanvasDOM(
-          editorContent.getBoundingClientRect().left -
-            scroller.getBoundingClientRect().left,
-          canvas,
-          pulseTheme ?? "light"
-        )
-      );
-    });
-  }, [isDrawing, pulseTheme]);
 
   // Update view upon view document changes
   useEffect(() => {
@@ -331,36 +260,6 @@ export default function CodeEditorView() {
     return lines.join("\n");
   }
 
-  function getDrawingLocation(line: DrawnLine): {
-    lineStart: number;
-    lineEnd: number;
-  } {
-    const points = line.points;
-    const pointsY = points
-      .map((point) => point.y)
-      .filter((point) => typeof point === "number");
-    const minY = Math.min(...pointsY);
-    const maxY = Math.max(...pointsY);
-    const editorContent = cmRef.current?.view?.contentDOM;
-    if (!editorContent) {
-      throw new Error("Editor content not found");
-    }
-    // Go through each line and column to find the start and end
-    const cmView = cmRef.current?.view;
-    const cmState = cmRef.current?.state;
-    const lineStartBlock = cmView?.lineBlockAtHeight(minY);
-    const lineStart = lineStartBlock
-      ? cmState?.doc.lineAt(lineStartBlock.from).number ?? -1
-      : -1;
-    const lineEndBlock = cmView?.lineBlockAtHeight(maxY);
-    const lineEnd = lineEndBlock
-      ? cmState?.doc.lineAt(lineEndBlock.from).number ?? -1
-      : -1;
-    return {
-      lineStart: lineStart,
-      lineEnd: lineEnd,
-    };
-  }
 
   function onContentChange(value: string) {
     setViewDocument((prev) => {
@@ -388,70 +287,9 @@ export default function CodeEditorView() {
     });
   }
 
-  const onTextExtracted = useCallback((line: DrawnLine, text: string) => {
-    // Get location information
-    const editorContent = cmRef.current?.view?.contentDOM;
-    if (!editorContent) {
-      throw new Error("Editor content not found");
-    }
-    const location = getDrawingLocation(line);
-    const newInfo: TextFileSelection = {
-      lineStart: location.lineStart,
-      lineEnd: location.lineEnd,
-      text,
-    };
-    setViewDocument((prev) => {
-      // Return undefined if viewDocument is not set
-      if (!prev) {
-        return prev;
-      }
-      return {
-        ...prev,
-        selections: [...(prev.selections ?? []), newInfo],
-      };
-    });
-  }, []);
-
-  function getCanvasDOM(
-    offsetX: number,
-    editorCanvas: HTMLCanvasElement,
-    theme: string
-  ) {
-    function getCanvas(editorCanvas: HTMLCanvasElement, theme: string) {
-      return (
-        <div
-          className="absolute left-0 top-0"
-          style={{
-            height: cmRef.current?.view?.contentDOM.offsetHeight,
-            width: cmRef.current?.view?.contentDOM.offsetWidth,
-          }}
-        >
-          <CanvasEditor
-            onTextExtracted={onTextExtracted}
-            isDownloadClip={isDownloadClip ?? false}
-            isDrawHulls={isDrawHulls ?? false}
-            editorCanvas={editorCanvas}
-            theme={theme}
-          />
-        </div>
-      );
-    }
-    const container = document.createElement("div");
-    container.id = "canvas-widget";
-    container.style.position = "absolute";
-    container.style.left = offsetX + "px";
-    const root = createRoot(container);
-    const canvas = getCanvas(editorCanvas, theme);
-    root.render(canvas);
-    return container;
-  }
-
   return (
     <div
       className="relative h-full w-full overflow-hidden rounded-lg bg-content2"
-      style={{
-        cursor: isDrawing && !isCanvasReady ? "wait" : "auto",
-      }}
     >
       {
         <ReactCodeMirror
